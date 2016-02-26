653c6bc1875e12e2edc7c2096f6dfacdc9e9f471
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-26 19:48:04 -0300

    Adicionando TesteSteps

---
+++ test/Ticket4S.SpecTests/AHomePageDoSiteDeveExibirOsEventosSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Ticket4S.SpecTests
+{
+    [Binding]
+    public class AHomePageDoSiteDeveExibirOsEventosSteps
+    {
+        [When(@"Entar na Pagina Principal do Site")]
+        public void QuandoEntarNaPaginaPrincipalDoSite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"o resultado deve conter uma lista de eventos disponiveis")]
+        public void EntaoOResultadoDeveConterUmaListaDeEventosDisponiveis()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"a Listagem deve conter somente eventos futuros")]
+        public void EntaoAListagemDeveConterSomenteEventosFuturos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"quando um evento ainda tem ingressos deve ser exibito a opcão de comprar ingresso")]
+        public void EntaoQuandoUmEventoAindaTemIngressosDeveSerExibitoAOpcaoDeComprarIngresso()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"quando um evento já se encontra esgotado deve ser exibito mas sem a opcão de comprar ingresso")]
+        public void EntaoQuandoUmEventoJaSeEncontraEsgotadoDeveSerExibitoMasSemAOpcaoDeComprarIngresso()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ test/Ticket4S.SpecTests/OUsuarioPodeComprarIngressosParaUmEventosSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Ticket4S.SpecTests
+{
+    [Binding]
+    public class OUsuarioPodeComprarIngressosParaUmEventosSteps
+    {
+        [When(@"O Usuário selecionar um Evento")]
+        public void QuandoOUsuarioSelecionarUmEvento()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"o Usário escolher um Tipo de Ingresso disponível")]
+        public void QuandoOUsarioEscolherUmTipoDeIngressoDisponivel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"o Usuário estiver deslogado")]
+        public void QuandoOUsuarioEstiverDeslogado()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"o Usuário estiver logado")]
+        public void QuandoOUsuarioEstiverLogado()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"os detalhes do Evento devem ser exibidos")]
+        public void EntaoOsDetalhesDoEventoDevemSerExibidos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"os Tipos de Ingressos devem ser todos listados em conjunto com a informação se ainda estão disponiveis para compra\.")]
+        public void EntaoOsTiposDeIngressosDevemSerTodosListadosEmConjuntoComAInformacaoSeAindaEstaoDisponiveisParaCompra_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"ele será solicitado que se logue")]
+        public void EntaoEleSeraSolicitadoQueSeLogue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"ele irá confirmar a compra de ingresso")]
+        public void EntaoEleIraConfirmarACompraDeIngresso()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

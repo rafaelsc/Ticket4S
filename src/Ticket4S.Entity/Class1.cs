7666533dbf61fd48c23777a625a06f4319fc527b
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-26 12:49:55 -0300

    Comite Inicial. Estrutura Inical do Projeto

---
+++ src/Ticket4S.Entity/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket4S.Entity
+{
+    public class Class1
+    {
+    }
+}

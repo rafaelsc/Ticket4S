6c7152670a718d1f57bb99952ddc4c1c6b6615b2
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-27 13:31:11 -0300

    Mais Testes

---
+++ test/Ticket4S.WebTests/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using FluentAssertions.Mvc;
+using FluentAssertions;
+using Ticket4S.Web.Controllers;
+using Xunit;
+
+namespace Ticket4S.WebTests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeDeveTrazerListaDeEventos()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            result.Should().BeViewResult();
+        }
+    }
+}

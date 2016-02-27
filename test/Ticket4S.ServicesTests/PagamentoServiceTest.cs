3734e60515e64187ccda3b20d79d80b7176084de
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-27 19:09:03 -0300

    Novo projeto de Teste para os Servicos

---
+++ test/Ticket4S.ServicesTests/PagamentoServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Ticket4S.MundipaggService;
+using Ticket4S.Services.Pagamento;
+using Xunit;
+
+namespace Ticket4S.ServicesTests
+{
+    public class PagamentoServiceTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+            //IPagamentoService pagamentoService = new MundpaggPagamentoService();
+
+            // Assert
+        }
+    }
+}

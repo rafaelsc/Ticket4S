d71af7188aeac3455b28f49c6f7bebc19cfe10a8
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-28 13:16:43 -0300

    Inicio do Email Service

---
+++ src/Ticket4S.Services/Email/IEmailService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Ticket4S.Services.Email.Model;
+using Ticket4S.Utils;
+
+namespace Ticket4S.Services.Email
+{
+    public interface IEmailService
+    {
+        [NotNull]
+        Task EnviarEmailAsync([NotNull] EMailAEnviar dadosDoEmail);
+    }
+
+    public class NullEmailService : IEmailService
+    {
+        public Task EnviarEmailAsync(EMailAEnviar dadosDoEmail)
+        {
+            Contract.Requires(dadosDoEmail != null);
+            Contract.Ensures(Contract.Result<Task>() != null);
+            ValidatorHelper.ThrowesIfNotValid(nameof(dadosDoEmail), dadosDoEmail);
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/Ticket4S.Utils/ValidatorHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Ticket4S.Utils
+{
+    public static class ValidatorHelper
+    {
+        public static void ThrowesIfNotValid(string paramName, object objectToValidate)
+        {
+            if(!TryValidate(objectToValidate))
+                throw new ArgumentException("Validacao dos dados do Paramentro Invalida.", paramName); //TODO: Validar melhor, Informar detalhes do erro na mensagem //TODO: Fix Typo
+        }
+
+        public static bool TryValidate(object @object)
+        {
+            ICollection<ValidationResult> results;
+            TryValidate(@object, out results);
+            return !results.Any();
+        }
+
+        public static bool TryValidate(object @object, out ICollection<ValidationResult> results)
+        {
+            var context = new ValidationContext(@object, serviceProvider: null, items: null);
+            results = new List<ValidationResult>();
+            return Validator.TryValidateObject(@object, context, results, validateAllProperties: true
+            );
+        }
+    }
+}

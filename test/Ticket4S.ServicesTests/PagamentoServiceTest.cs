e2a65080068b0a6c42a4a73d26c65aee765416c4
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-28 09:27:45 -0300

    Novos Testes

--- test/Ticket4S.ServicesTests/PagamentoServiceTest.cs
+++ test/Ticket4S.ServicesTests/PagamentoServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using AutoMapper;
 using Ticket4S.MundipaggService;
 using Ticket4S.Services.Pagamento;
 using Xunit;
@@ -8,15 +9,73 @@ namespace Ticket4S.ServicesTests
 {
     public class PagamentoServiceTest
     {
+        private readonly IMapper _mapper;
+        
+        public PagamentoServiceTest()
+        {
+            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperConfiguration>());
+            _mapper = mapperConfig.CreateMapper();
+        }
+
         [Fact]
-        public void TestMethod1()
+        public void CobrancaNoCartaoComResultadoAprovado()
         {
             // Arrange
 
+
             // Act
-            //IPagamentoService pagamentoService = new MundpaggPagamentoService();
+            IPagamentoService pagamentoService = new MundpaggPagamentoService(_mapper);
+            //pagamentoService.PagarComCartaoDeCredito(cobrancaData);
 
             // Assert
+        }
+
+        [Fact]
+        public void CobrancaNoCartaoComResultadoNaoAtorizado()
+        {
+            // Arrange
+           
+            // Act
+            
+            // Assert
+            
+        }
+
+        [Fact]
+        public void CobrancaNoCartaoComFalhaDeComunicacao()
+        {
+            // Arrange
+            
+
+            // Act
+            
+
+            // Assert
+          
+        }
+
+        [Fact]
+        public void CobrancaNoCartaoFaltandoDados1()
+        {
+            // Arrange
+        
+
+            // Act
+           
+            // Assert
+         
+        }
+
+        [Fact]
+        public void CobrancaNoCartaoFaltandoDados2()
+        {
+            // Arrange
+            
+
+            // Act
+           
+            // Assert
+           
         }
     }
 }

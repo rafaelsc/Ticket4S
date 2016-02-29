9ac214492044f396250816d2b37a8fcbd89f6391
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-28 22:05:50 -0300

    Adding AddAll

---
+++ src/Ticket4S.Extensions/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Ticket4S.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void AddAll<T>(this ICollection<T> col, IEnumerable<T> itens) 
+        {
+            foreach (var item in itens)
+            {
+                col.Add(item);
+            }
+        }
+    }
+}

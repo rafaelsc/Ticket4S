8c072525af23f493ae1c320ab060143530d8cf09
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-03-01 20:04:30 -0300

    Traduzindo codigo para Ingles

---
+++ src/Ticket4S.Entity.Querys/EventQuerys.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Ticket4S.Entity.Querys
+{
+    public static class EventQuerys
+    {
+        public static IQueryable<Event.Event> OnlyActives(this IQueryable<Event.Event> events)
+        {
+            return events.Where(e => e.Active);
+        }
+
+        public static IQueryable<Event.Event> ListAvailableEventsWithTicket(this IQueryable<Event.Event> events)
+        {
+            var date = DateTimeOffset.Now;
+            return events.ListAvailableEventsWithTicket(date);
+        }
+
+        public static IQueryable<Event.Event> ListAvailableEventsWithTicket(this IQueryable<Event.Event> events, DateTimeOffset onDate)
+        {
+            var result = from ev in events.OnlyActives()
+                                    .Include(e => e.TicketsTypes)
+                         where onDate >= ev.BeginningOfSales
+                         where ev.EndOfSales == null || ev.EndOfSales <= onDate
+                         where ev.TicketsTypes.Any()
+                         orderby ev.Name
+                         select ev;
+
+            return result;
+        }
+    }
+
+    public static class EventTicketTypesQuerys
+    {
+        public static IQueryable<Event.EventTicketType> OnlyActives(this IQueryable<Event.EventTicketType> eventTicketTypes)
+        {
+            return eventTicketTypes.Where(e => e.Available);
+        }
+
+        public static IQueryable<Event.EventTicketType> InDefaultOrder(this IQueryable<Event.EventTicketType> eventTicketTypes)
+        {
+            var result = from t in eventTicketTypes
+                         orderby t.ViewOrder
+                         orderby t.Name
+                         select t;
+
+            return result;
+        }
+    }
+
+}
--- src/Ticket4S.Entity.Querys/GeoQuerys.cs
+++ src/Ticket4S.Entity.Querys/GeoQuerys.cs
@@ -4,25 +4,25 @@ using Ticket4S.Entity.Geo;
 
 namespace Ticket4S.Entity.Querys
 {
-    public static class UFsQuerys
+    public static class StatesQuerys
     {
-        public static IQueryable<State> Ativos(this IQueryable<State> ufs)
+        public static IQueryable<State> OnlyAvailables(this IQueryable<State> states)
         {
-            return ufs.Where(e => e.Available);
+            return states.Where(e => e.Available);
         }
 
-        public static IQueryable<State> OrdemPadrao(this IQueryable<State> ufs)
+        public static IQueryable<State> InDefaultOrder(this IQueryable<State> states)
         {
-            var result = from t in ufs
+            var result = from t in states
                 orderby t.Name
                 select t;
 
             return result;
         }
 
-        public static IQueryable<State> ListarParaCombo(this IQueryable<State> ufs)
+        public static IQueryable<State> ListForDropDown(this IQueryable<State> states)
         {
-            var result = from t in ufs.AsNoTracking().Ativos().OrdemPadrao()
+            var result = from t in states.AsNoTracking().OnlyAvailables().InDefaultOrder()
                 select t;
 
             return result;
@@ -31,23 +31,23 @@ namespace Ticket4S.Entity.Querys
 
     public static class CidadeQuerys
     {
-        public static IQueryable<City> Ativos(this IQueryable<City> cidades)
+        public static IQueryable<City> OnlyAvailables(this IQueryable<City> cities)
         {
-            return cidades.Where(e => e.Available);
+            return cities.Where(e => e.Available);
         }
 
-        public static IQueryable<City> OrdemPadrao(this IQueryable<City> cidades)
+        public static IQueryable<City> InDefaultOrder(this IQueryable<City> cities)
         {
-            var result = from t in cidades
+            var result = from t in cities
                          orderby t.Name
                          select t;
 
             return result;
         }
 
-        public static IQueryable<City> ListarParaCombo(this IQueryable<City> cidades)
+        public static IQueryable<City> ListForDropDown(this IQueryable<City> cities)
         {
-            var result = from t in cidades.AsNoTracking().Ativos().OrdemPadrao()
+            var result = from t in cities.AsNoTracking().OnlyAvailables().InDefaultOrder()
                          select t;
 
             return result;
@@ -56,23 +56,23 @@ namespace Ticket4S.Entity.Querys
 
     public static class BairroQuerys
     {
-        public static IQueryable<District> Ativos(this IQueryable<District> bairros)
+        public static IQueryable<District> OnlyAvailables(this IQueryable<District> districts)
         {
-            return bairros.Where(e => e.Available);
+            return districts.Where(e => e.Available);
         }
 
-        public static IQueryable<District> OrdemPadrao(this IQueryable<District> bairros)
+        public static IQueryable<District> InDefaultOrder(this IQueryable<District> districts)
         {
-            var result = from t in bairros
+            var result = from t in districts
                          orderby t.Name
                          select t;
 
             return result;
         }
 
-        public static IQueryable<District> ListarParaCombo(this IQueryable<District> bairros)
+        public static IQueryable<District> ListForDropDown(this IQueryable<District> districts)
         {
-            var result = from t in bairros.AsNoTracking().Ativos().OrdemPadrao()
+            var result = from t in districts.AsNoTracking().OnlyAvailables().InDefaultOrder()
                          select t;
 
             return result;

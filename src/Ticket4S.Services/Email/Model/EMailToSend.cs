35defc7687deaf01b77d099ef304cae4fec66aab
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-02-29 23:55:13 -0300

    Traduzindo codigo para Ingles

---
+++ src/Ticket4S.Services/Email/Model/EMailToSend.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket4S.Services.Email.Model
+{
+    public class EMailToSend
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required, EmailAddress]
+        public string DestinationEMail { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+
+        public override string ToString() => $"DestinationEMail: {DestinationEMail}, Subject: {Subject}";
+    }
+}
---
+++ src/Ticket4S.Services/Pagamento/Model/PaymentResult.cs
@@ -0,0 +1,24 @@
+namespace Ticket4S.Services.Pagamento.Model
+{
+    public class PaymentResult
+    {
+        public PaymentResult(bool paymentBilledSuccessful, string orderIdInTheGatewaySystem, string operationMessage, string debugRawData)
+        {
+            PaymentBilledSuccessful = paymentBilledSuccessful;
+            OrderIdInTheGatewaySystem = orderIdInTheGatewaySystem;
+            OperationMessage = operationMessage;
+            DebugRawData = debugRawData;
+        }
+
+        public bool PaymentBilledSuccessful { get; }
+        
+        public string OrderIdInTheGatewaySystem { get; }
+
+        public string OperationMessage { get; }
+
+        public string DebugRawData { get; }
+
+
+        public override string ToString() => $"PaymentBilledSuccessful: {PaymentBilledSuccessful}, OrderIdInTheGatewaySystem: {OrderIdInTheGatewaySystem}, OperationMessage: {OperationMessage}";
+    }
+}

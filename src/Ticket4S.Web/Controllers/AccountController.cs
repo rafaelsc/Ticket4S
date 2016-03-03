8ff64581bf2de5d937e2a68d332642f56f485f92
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2016-03-03 02:51:44 -0300

    Cadastro de Usuario Completo

--- src/Ticket4S.Web/Controllers/AccountController.cs
+++ src/Ticket4S.Web/Controllers/AccountController.cs
@@ -146,24 +146,32 @@ namespace Ticket4S.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                var user = new User { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+            if (!ModelState.IsValid)
+                return View(model);
 
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
+            var user = new User
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                Name = model.Name,
+                CPF = model.CPF,
+                DateOfBirth = model.DateOfBirth,
+                Gender = model.Gender
+            };
+            var result = await UserManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
+                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                // Send an email with this link
+                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                return RedirectToAction("Index", "Home");
             }
+            AddErrors(result);
 
             // If we got this far, something failed, redisplay form
             return View(model);
--- src/Ticket4S.Web/Models/AccountViewModels.cs
+++ src/Ticket4S.Web/Models/AccountViewModels.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Ticket4S.Entity.Geo;
+using Ticket4S.Entity.User;
 
 namespace Ticket4S.Web.Models
 {
@@ -72,13 +76,57 @@ namespace Ticket4S.Web.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Senha")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirmação de Senha")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+
+        [Required]
+        [Display(Name = "Nome")]
+        public virtual string Name { get; set; }
+
+        [Required]
+        [Display(Name = "CPF")]
+        public virtual string CPF { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data Nascimento")]
+        public virtual DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Sexo")]
+        public virtual Gender Gender { get; set; }
+
+        //[Required]
+        public virtual AddressViewModel Address { get; set; }
+    }
+
+    public class AddressViewModel
+    {
+        [Required]
+        public virtual string StateId { get; set; }
+        [Required]
+        public virtual string CityId { get; set; }
+        [Required]
+        public virtual string DistrictId { get; set; }
+        
+
+        [Required, StringLength(8)]
+        public virtual string ZipCode { get; set; }
+
+        [Required, StringLength(256)]
+        public virtual string Street { get; set; }
+
+        [Required, StringLength(32)]
+        public virtual string StreetNumber { get; set; }
+
+        [StringLength(128)]
+        public virtual string StreetComplement { get; set; }
     }
 
     public class ResetPasswordViewModel
